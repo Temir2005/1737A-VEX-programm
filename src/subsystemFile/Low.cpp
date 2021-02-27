a37bc301dfe16ff49261e515b0c40fa08448a375
Temir2005 <51045196+Temir2005@users.noreply.github.com>
2021-02-28 03:27:23 +0600

    Update Low.cpp

--- src/subsystemFile/Low.cpp
+++ src/subsystemFile/Low.cpp
@@ -1,13 +1,35 @@
 #include "main.h"
-using namespace pros;
 
-void setLow(int power){
-  krutilkaLow = power;
+
+void setAll(int power1){
+  krutilkaHigh = power1;
+  krutilkaLow = power1;
+}
+void setIntakes(int power){
   intakeLeft = power;
   intakeRight = power;
 }
-
 void setAllMotors(){
-    int power = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
+    int power = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
     setAll(power);
+    setIntakes(power);
+}
+
+void allkrutilkas(int degrees, int voltage){
+  int direction = abs(degrees) / degrees;
+  krutilkaHigh.tare_position();
+  while(abs(krutilkaHigh.get_position()) < abs(degrees)){
+    setAll(voltage * direction);
+    pros::delay(10);
+  }
+  setAll(0);
+}
+void intakes(int degrees, int voltage){
+  int direction = abs(degrees) / degrees;
+  intakeLeft.tare_position();
+  while(abs(intakeLeft.get_position()) < abs(degrees)){
+    setIntakes(voltage * direction);
+    pros::delay(10);
+  }
+  setIntakes(0);
 }
